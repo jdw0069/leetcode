4e89c98f8a4dcd97018821c5750e3ddaa9590998
jdw0069 <jdw0069@auburn.edu>
2022-05-30 21:27:54 -0500

    brute force solution

---
+++ week1/tests_twoSum.py
@@ -0,0 +1,26 @@
+import os
+import pytest
+from twoSum import Solution
+
+solution = Solution()
+
+def test_example1():
+    nums = [2,7,11,15] 
+    target = 9
+    actual = solution.twoSum(nums, target)
+    expected = [0,1]
+    assert expected == actual
+
+def test_example2():
+    nums = [3,2,4] 
+    target = 6
+    actual = solution.twoSum(nums, target)
+    expected = [1,2]
+    assert expected == actual
+
+def test_example3():
+    nums = [3,3] 
+    target = 6
+    actual = solution.twoSum(nums, target)
+    expected = [0,1]
+    assert expected == actual
